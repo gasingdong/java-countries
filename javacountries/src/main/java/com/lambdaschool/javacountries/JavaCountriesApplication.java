b15af92b24fc0e808510eff595a78609ffd93c89
gasingdong <gasingdong@gmail.com>
2019-09-09 21:02:24 -0700

    create CountryList instance

--- javacountries/src/main/java/com/lambdaschool/javacountries/JavaCountriesApplication.java
+++ javacountries/src/main/java/com/lambdaschool/javacountries/JavaCountriesApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaCountriesApplication {
 
+  static CountryList countryList;
+
   public static void main(String[] args) {
+    countryList = new CountryList();
     SpringApplication.run(JavaCountriesApplication.class, args);
   }
-
 }
